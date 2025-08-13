c7e4dff59838e39e5d8f1d40347b3a1923863624
Jeff <110954699+jshriner6@users.noreply.github.com>
2025-08-13 11:55:28 +0000

    Latest build deployed.

--- notes/lunr-pretext-search-index.js
+++ notes/lunr-pretext-search-index.js
@@ -1,22 +1,13 @@
 var ptx_lunr_search_style = "textbook";
 var ptx_lunr_docs = [
 {
-  "id": "notes-week-01",
+  "id": "sec-1-1",
   "level": "1",
-  "url": "notes-week-01.html",
+  "url": "sec-1-1.html",
   "type": "Section",
   "number": "",
-  "title": "Week 1",
-  "body": " Week 1  This week we covered the following topics:  "
-},
-{
-  "id": "notes-week-02",
-  "level": "1",
-  "url": "notes-week-02.html",
-  "type": "Section",
-  "number": "",
-  "title": "Week 2",
-  "body": " Week 2  This week we covered the following topics in the second week.  "
+  "title": "<span class=\"process-math\">\\(\\S 1.1\\text{:}\\)<\/span> What Can We Expect",
+  "body": " : What Can We Expect  This week we covered the following topics:  "
 }
 ]
 
